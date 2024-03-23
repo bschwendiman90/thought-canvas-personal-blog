70c09ce14e358a3add7a9d4f5b96c3fb6ab5e810
Ben Schwendiman <bschwendiman90@gmail.com>
2024-03-23 17:27:33 -0600

    final version

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,20 +1,14 @@
 
 const themeBtn = document.getElementById('theme');
 const homeBtn = document.getElementById('homeBtn')
-// Retrieve the array of objects from local storage
+
 let blogPosts = JSON.parse(localStorage.getItem('blogPosts'));
 
-// Check if the array exists and is not empty
+
 if (blogPosts && blogPosts.length > 0) {
-    // Iterate over the array
     blogPosts.forEach(function(object) {
-        // Create a new div element for each object
         let newDiv = document.createElement('div');
-        
-        // Set the content of the new div using the object properties
         newDiv.innerHTML = `<p>Username: ${object.username}</p><p>Title: ${object.title}</p><p>Content: ${object.content}</p>`;
-        
-        // Append the new div to the body of the page
         document.body.appendChild(newDiv);
     });
 };
