6edef1b64d92f4208f39d571d331a90ab58f4b1a
Ben Schwendiman <bschwendiman90@gmail.com>
2024-03-23 12:10:23 -0600

    initial form action

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
-<span class="material-symbols-outlined">
+{/* <span class="material-symbols-outlined">
             dark_mode
-        </span>
+        </span> */}
+
+const themeBtn = document.getElementById('theme');
+const usernameEl = document.getElementById('username');
+const titleEl = document.getElementById('title');
+const contentEl = document.getElementById('content')
+const submitEl = document.getElementById('submit')
+
+
+submitEl.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let blogPost = {
+    username: usernameEl.value,
+    title: titleEl.value,
+    content: contentEl.value
+};
+
+
+localStorage.setItem('blogPost', JSON.stringify(blogPost))
+});
