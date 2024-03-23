45a212db484da1eae51329c6f964d71ac0c8780c
Ben Schwendiman <bschwendiman90@gmail.com>
2024-03-23 17:14:53 -0600

    final theme change

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,20 +1,43 @@
 
 const themeBtn = document.getElementById('theme');
+const homeBtn = document.getElementById('homeBtn')
+// Retrieve the array of objects from local storage
+let blogPosts = JSON.parse(localStorage.getItem('blogPosts'));
 
+// Check if the array exists and is not empty
+if (blogPosts && blogPosts.length > 0) {
+    // Iterate over the array
+    blogPosts.forEach(function(object) {
+        // Create a new div element for each object
+        let newDiv = document.createElement('div');
+        
+        // Set the content of the new div using the object properties
+        newDiv.innerHTML = `<p>Username: ${object.username}</p><p>Title: ${object.title}</p><p>Content: ${object.content}</p>`;
+        
+        // Append the new div to the body of the page
+        document.body.appendChild(newDiv);
+    });
+};
+console.log(blogPosts)
 
-themeBtn.addEventListener('click', function(event) {
+homeBtn.addEventListener('click', function () {
+    window.location.href = './index.html'
+})
+
+themeBtn.addEventListener('click', function() {
     if (document.querySelector('body').getAttribute('id') === 'dark'){
-       document.querySelector('body').removeAttribute('id', 'dark')
-       document.getElementById('moon').id = 'sun';
-       document.getElementById('theme').innerHTML = 'light_mode'
-   }
-       else {
+        document.querySelector('body').removeAttribute('id', 'dark')
+        document.getElementById('theme').innerHTML = 'light_mode'
         let divs = document.querySelectorAll('div');
-           document.querySelector('body').setAttribute('id', 'dark')
-           for (let i = 0; i < array.length; i++) {
-                divs[i].setAttribute('id', 'darkDiv');
+        for (let i = 0; i < divs.length; i++) {
+             divs[i].removeAttribute('id', 'darkDiv');
            }
-           
-           document.getElementById('theme').innerHTML = 'dark_mode'
+   } else {
+        document.querySelector('body').setAttribute('id', 'dark')
+        document.getElementById('theme').innerHTML = 'dark_mode'
+        let divs = document.querySelectorAll('div');
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].setAttribute('id', 'darkDiv');
+           }
    }
    });
